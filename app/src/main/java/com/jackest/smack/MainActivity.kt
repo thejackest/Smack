5300aaa68092e8832b4776bbfedfbd9b5f0cc8d9
jackest.gong <jackest.gong@bluelab.com>
2019-09-24 10:30:14 +1200

    init commit

---
+++ app/src/main/java/com/jackest/smack/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.jackest.smack
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+class MainActivity : AppCompatActivity() {
+
+//    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val toggle  = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, "Open navigation drawer","Close navigation drawer")
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+//        val fab: FloatingActionButton = findViewById(R.id.fab)
+//        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show() //similar to toast, pop up message
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)){
+            drawer_layout.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
+        }
+    }
+}
+
