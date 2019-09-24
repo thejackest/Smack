b0d43994ba9d582a7d787c9e5c5f4372d5fc148e
jackest.gong <jackest.gong@bluelab.com>
2019-09-25 10:42:09 +1200

    spinner created and broadcast setup

--- app/src/main/java/com/jackest/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/jackest/smack/Controller/CreateUserActivity.kt
@@ -1,12 +1,16 @@
 package com.jackest.smack.Controller
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.jackest.smack.R
 import com.jackest.smack.Services.AuthService
 import com.jackest.smack.Services.UserDataService
+import com.jackest.smack.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -17,6 +21,8 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+
+        createSpinner.visibility = View.INVISIBLE
     }
     fun generateUserAvatar(view: View){
         val random = Random()
@@ -47,29 +53,57 @@ class CreateUserActivity : AppCompatActivity() {
 
     }
     fun createUserClicked(view: View){
+        enableSpinner(true)
         val userName = createNameText.text.toString()
         val email = createEmailText.text.toString()
         val password = createPasswordText.text.toString()
 
-        AuthService.regisiterUser(this,"dhaj","sda"){
-            registerSuccess ->
-            if (registerSuccess){
-                AuthService.loginUser(this, email,password){
-                    loginSuccess ->
-                    if (loginSuccess){
-                        println(AuthService.authToken)
-                        println(AuthService.userEmail)
-                        AuthService.createUser(this,userName, email, userAvatar,avatarColor){createSuccess ->
-                            if(createSuccess){
-                                println(UserDataService.avatarName)
-                                println(UserDataService.avatarColor)
-                                println(UserDataService.name)
-                                finish()
+        if (userName.isNotEmpty()&&email.isNotEmpty()&&password.isNotEmpty()){
+            AuthService.regisiterUser(this,email,password){
+                    registerSuccess ->
+                if (registerSuccess){
+                    AuthService.loginUser(this, email,password){
+                            loginSuccess ->
+                        if (loginSuccess){
+                            println(AuthService.authToken)
+                            println(AuthService.userEmail)
+                            AuthService.createUser(this,userName, email, userAvatar,avatarColor){createSuccess ->
+                                if(createSuccess){
+                                    //println(UserDataService.avatarName)
+                                    //println(UserDataService.avatarColor)
+                                    //println(UserDataService.name)
+
+                                    //tell the main activity what's going on
+                                    val userDataChange  =Intent(BROADCAST_USER_DATA_CHANGE)
+                                    LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+
+                                    enableSpinner(false)
+                                    finish()
+                                }else{errorToast()}
                             }
-                        }
+                        }else{errorToast()}
                     }
-                }
+                }else{errorToast()}
             }
+        }else{
+            Toast.makeText(this,"Make sure user name, email and password are filled in.", Toast.LENGTH_LONG).show()
+            enableSpinner(false)
         }
+    }
+
+    fun errorToast(){
+        Toast.makeText(this, "Something went wrong, please try again.", Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean){
+        if(enable){
+            createSpinner.visibility = View.VISIBLE
+        }else{
+            createSpinner.visibility = View.INVISIBLE
+        }
+        createUserBtn.isEnabled = !enable
+        createAvatarimageView.isEnabled = !enable
+        generateBackgroundColorBtn.isEnabled = !enable
     }
 }
--- app/src/main/java/com/jackest/smack/Utilities/Constants.kt
+++ app/src/main/java/com/jackest/smack/Utilities/Constants.kt
@@ -8,3 +8,6 @@ const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
 
 const val URL_CREATE_USER = "${BASE_URL}account/add"
+
+//Broadcast constants
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
