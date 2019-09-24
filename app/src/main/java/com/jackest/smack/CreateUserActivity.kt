d82597fad4cdfecf1f5b713675dd30f6605d76c9
jackest.gong <jackest.gong@bluelab.com>
2019-09-24 13:07:04 +1200

    generate avatar and background color

--- app/src/main/java/com/jackest/smack/CreateUserActivity.kt
+++ app/src/main/java/com/jackest/smack/CreateUserActivity.kt
@@ -1,19 +1,46 @@
 package com.jackest.smack
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
 
+    var userAvatar  = "profileDefault"
+    var avatarColor = "[0.5,0.5,0.5,1]"//api for mac, between 0-1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
     }
     fun generateUserAvatar(view: View){
+        val random = Random()
+        val color = random.nextInt(2)//0,1
+        val avatar = random.nextInt(28)//0-27
 
+        userAvatar = if (color==0){
+            "light$avatar"
+        }else{
+            "dark$avatar"
+        }
+
+        val resourceId = resources.getIdentifier(userAvatar,"drawable",packageName)
+        createAvatarimageView.setImageResource(resourceId)
     }
     fun generateBackgroundColorClicked(view: View){
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+
+        createAvatarimageView.setBackgroundColor(Color.rgb(r,g,b))
+
+        val savedR = r.toDouble()/255
+        val savedG = g.toDouble()/255
+        val savedB = b.toDouble()/255
+        avatarColor = "[$savedR,$savedG,$savedB,1]"
 
     }
     fun createUserClicked(view: View){
